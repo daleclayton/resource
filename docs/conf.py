be991b330399349f4e666029221f171723d3e4e7
daleclayton <nedhollister@hotmail.com>
2025-08-09 15:18:54 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -4,9 +4,9 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('.'))
 
-project = 'Mycomcast Helpline'
+project = 'Comcast Resources'
 copyright = '2025'
-author = 'John Otterbein'
+author = 'Bernard Benesh'
 
 release = '1.0'
 
@@ -21,3 +21,4 @@ exclude_patterns = []
 
 html_theme = 'alabaster'
 html_static_path = ['_static']
+

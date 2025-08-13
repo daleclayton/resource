e8807ff678a6028455e37d5a208f4bf3f748ed5e
daleclayton <nedhollister@hotmail.com>
2025-08-13 10:44:14 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -4,9 +4,9 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('.'))
 
-project = 'Comcast Resources'
+project = 'Authority-Precomcast'
 copyright = '2025'
-author = 'Bernard Benesh'
+author = 'Jalkis Raik'
 
 release = '1.0'
 
@@ -21,4 +21,5 @@ exclude_patterns = []
 
 html_theme = 'alabaster'
 html_static_path = ['_static']
+
 
